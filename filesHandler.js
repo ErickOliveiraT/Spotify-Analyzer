5a177ee1aa95d570cbc1965fbd19943671d85b3a
Erick <erick.teixeira_50@hotmail.com>
2020-04-02 01:55:22 -0300

    Format genre name

--- filesHandler.js
+++ filesHandler.js
@@ -1,6 +1,24 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 module.exports = {
+
+    formatGenreName(genre) {
+        let words = genre.split(' ');
+        let name = new String;
+        if (words.length == 1) {
+            name = genre.charAt(0).toUpperCase();
+            for (let i = 1; i < genre.length; i++) name += genre.charAt(i);
+        }
+        else {
+            words.forEach((word, index) => {
+                let _name = word.charAt(0).toUpperCase();
+                for (let i = 1; i < word.length; i++) _name += word.charAt(i);
+                name += _name; 
+                if (index < words.length) name += ' ';
+            });
+        }
+        return name;
+    },
   
     async writeStats(data, format) {
         if (format == 'pt-br') {
@@ -8,6 +26,7 @@ module.exports = {
                 element.percentage = element.percentage.toString().replace('.',',');
             });
         }
+        data.forEach((element) => {element.genre = this.formatGenreName(element.genre)})
         csvWriter = createCsvWriter({
             path: 'stats.csv',
             fieldDelimiter: ';',
