3e29a180ef67d7252c89b9398503326b72758a00
Erick <erick.teixeira_50@hotmail.com>
2020-04-02 00:01:37 -0300

    Limite de gêneros

--- genres.js
+++ genres.js
@@ -1,9 +1,16 @@
 const requests = require('./requests')
 
-async function analyzeGenres(time_range) {
+async function analyzeGenres(time_range, considerAll, genresLimit) {
+    var fav_artists = undefined;
     try {
-        let fav_artists = await requests.getArtists(time_range, 50);
-        let all_genres = [];
+        fav_artists = await requests.getArtists(time_range, 50);
+    }
+    catch (e) {
+        console.log('Erro ao carregar artisas mais ouvidos');
+        console.log(e)
+    }
+
+    let all_genres = [];
         for (let i = 0; i < fav_artists.length; i++) {
             for(let j = 0; j < fav_artists[i].genres.length; j++) all_genres.push(fav_artists[i].genres[j]);
         }
@@ -11,31 +18,52 @@ async function analyzeGenres(time_range) {
             return self.indexOf(elem) == pos;
         });
         let stats = [];
-        uniqueGenres.forEach((genre) => {
-            let count = 0;
-            for (i = 0; i < all_genres.length; i++) {
-                if (genre == all_genres[i]) count++;
+        if (considerAll) {
+            uniqueGenres.forEach((genre) => {
+                let count = 0;
+                for (i = 0; i < all_genres.length; i++) {
+                    if (genre == all_genres[i]) count++;
+                }
+                let genreInfo = {
+                    genre: genre,
+                    count: count,
+                    percentage: parseFloat(count*100/all_genres.length).toFixed(2)
+                }
+                stats.push(genreInfo);
+            });
+            stats.sort((a, b) => (a.count > b.count) ? -1 : 1);
+
+            return stats;
+        }
+        else {
+            uniqueGenres.forEach((genre) => {
+                let count = 0;
+                for (i = 0; i < all_genres.length; i++) {
+                    if (genre == all_genres[i]) count++;
+                }
+                let genreInfo = {
+                    genre: genre,
+                    count: count
+                }
+                stats.push(genreInfo);
+            });
+            stats.sort((a, b) => (a.count > b.count) ? -1 : 1);
+            let Totaloccurrences = 0;
+            for (let i = 0; i < genresLimit; i++) Totaloccurrences += stats[i].count;
+            let reductedStats = [];
+            for (let i = 0; i < genresLimit; i++) {
+                reductedStats.push({
+                    genre: stats[i].genre,
+                    count: stats[i].count,
+                    percentage: parseFloat(stats[i].count*100/Totaloccurrences).toFixed(2)
+                });
             }
-            let genreInfo = {
-                genre: genre,
-                count: count,
-                percentage: parseFloat(count*100/all_genres.length).toFixed(2)
-            }
-            stats.push(genreInfo);
-        });
-        stats.sort((a, b) => (a.count > b.count) ? -1 : 1);
-        //let bestFive = [];
-        //for (let i = stats.length-1; i > stats.length-6; i--) bestFive.push(stats[i]);
-        return stats;
-        //return bestFive;
-    }
-    catch (e) {
-        console.log('Erro ao carregar artisas mais ouvidos');
-        console.log(e)
-    }
+
+            return reductedStats;
+        }
 }
 
-analyzeGenres('long_term')
+analyzeGenres('long_term', false, 5)
 .then((res) => {
     res.forEach((genre) => {
         console.log(genre.genre + ': ' + genre.percentage + '%');
